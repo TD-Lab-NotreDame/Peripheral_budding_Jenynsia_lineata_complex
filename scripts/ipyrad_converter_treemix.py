c758a3d3ecdf0a543e5aeb7bd7d9d0039df82d46
TD-Lab-NotreDame <130703839+TD-Lab-NotreDame@users.noreply.github.com>
2025-06-03 14:50:00 -0400

    Update ipyrad_converter_treemix.py

--- scripts/ipyrad_converter_treemix.py
+++ scripts/ipyrad_converter_treemix.py
@@ -4,7 +4,7 @@ import pandas as pd
 # init a conversion tool
 converter = ipa.vcf_to_hdf5(
     name="filtered_final_94.recode",
-    data="filtered_final_94.recode.vcf.gz",
+    data="inputs/filtered_final_94.recode.vcf.gz",
     ld_block_size=500,
 )
 # run the converter
