85d1020d2f2b8119fbe1f9d9966aac9fb3ee6bf0
TD-Lab-NotreDame <130703839+TD-Lab-NotreDame@users.noreply.github.com>
2025-06-03 14:50:22 -0400

    Update ipyrad_converter_pca.py

--- scripts/ipyrad_converter_pca.py
+++ scripts/ipyrad_converter_pca.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # init a conversion tool
 converter = ipa.vcf_to_hdf5(
-    name="inputs/filtered_final_83.recode",
+    name="filtered_final_83.recode",
     data="inputs/filtered_final_83.recode.vcf.gz",
     ld_block_size=500,
 )
